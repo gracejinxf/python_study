50a91122542980dc5e46961f1d9741dc233279fd
gracejinxf <gracejinxf@gmail.com>
2020-07-11 09:55:00 -0500

    config debug (launch) json

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+first_number = int(input("Enter first number "))
+second_number = int(input("Enter second number "))
+
+print("\n")
+print("First Number:", first_number)
+print("Second Number:", second_number)
